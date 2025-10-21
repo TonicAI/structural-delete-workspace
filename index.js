a1ceafd42cab71a2ebc848f5cc846d9c2b3efe6c
Yuliia Korabelska <yuliia@tonic.ai>
2025-10-21 11:05:26 -0400

    add headers

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ async function run() {
     }
 
     const url = new URL(`/api/workspace/${encodeURIComponent(workspaceId)}`, apiHost);
+    const version = require('./package.json').version;
 
     const options = {
       hostname: url.hostname,
@@ -37,7 +38,10 @@ async function run() {
       path: url.pathname,
       method: 'DELETE',
       headers: {
-        'Authorization': `apikey ${apiKey}`
+        'Authorization': `apikey ${apiKey}`,
+        'User-Agent': 'Tonic-Github-Action',
+        'X-GitHub-Action': 'copy-workspace',
+        'X-GitHub-Action-Version': version
       }
     };
 
